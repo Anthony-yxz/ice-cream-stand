ca61a3d1dfa03c8812028c0b80abc430b10e518c
Anthony-yxz <ag537413@georgetownisd.org>
2026-02-12 14:54:59 -0600

    Update benchmark_4.py

--- benchmark_4.py
+++ benchmark_4.py
@@ -1,30 +1,17 @@
-# customer_class.py
-# The customer class represents a customer at the lemondade stand.
-# The customer has preferences for lemonade sweetness and ice level,
-# and a price point they are willing to pay.
-# All of these attributes will influence their purchasing decision and
-# are returned as a list of attributes.
+import customer_class
 
-import random
+from customer_class import Customer
 
-class Customer:
-    def __init__(self, ice_cream_preference=None, toppings_preference=None, price_point=None):
-        self.ice_cream_preference = (
-            ice_cream_preference if ice_cream_preference is not None
-            else random.randint(1, 3)
-        )
-        self.toppings_preference = (
-            toppings_preference if toppings_preference is not None
-            else random.randint(0, 3)
-        )
-        self.price_point = (
-            price_point if price_point is not None
-            else round(random.uniform(0.25, 4.0),2)
-        )
 
-    def get_customer_attributes(self):
-        return {
-            "Ice_Cream": self.ice_cream_preference,
-            "Toppings": self.toppings_preference,
-            "price": self.price_point
-        }
+customers = []
+
+for i in range(10):
+    customers.append(Customer())
+
+attributes = customer.get_customer_attributes()
+
+
+sweetness_pref = attributes["sweetness"]
+ice_pref = attributes["ice"]
+price_limit = attributes["price"]
+
