1cdb3907c3212be63596bed21b197036dcabb279
bradysauseda <bs536909@georgetownisd.org>
2026-02-25 15:23:06 -0600

    added storyline

---
+++ STORY_LINE.py
@@ -0,0 +1,4 @@
+input("LeBron James is running low on money...")
+input("He decides to open a icecream store...")
+input("He wants you to manange the place...")
+print("In return he will give you 5 million dollars!!!!")
