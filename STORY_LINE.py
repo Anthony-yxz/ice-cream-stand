812077c96e52d6acecc616b2a0bb08ad8584ac61
Anthony-yxz <ag537413@georgetownisd.org>
2026-02-26 10:22:03 -0600

    Update STORY_LINE.py

--- STORY_LINE.py
+++ STORY_LINE.py
@@ -1,4 +1,11 @@
-input("LeBron James is running low on money...")
-input("He decides to open a icecream store...")
-input("He wants you to manange the place...")
-print("In return he will give you 5 million dollars!!!!")
+import time
+
+while True:
+    print("lebron be running low on money.")
+    time.sleep(5) 
+    print("He decides to open a icecream store")
+    time.sleep(5)
+    print("He wants you to manange the place")
+    time.sleep(5)
+    print("In return he will give you 5 million dollars!")
+    break
