13d0ec7440081a7629e7fba8b20639eb099f7fd3
Anthony-yxz <ag537413@georgetownisd.org>
2026-02-27 14:53:07 -0600

    Update STORY_LINE.py

--- STORY_LINE.py
+++ STORY_LINE.py
@@ -2,10 +2,11 @@ import time
 
 while True:
     print("lebron be running low on money.")
-    time.sleep(5) 
+    time.sleep(3) 
     print("He decides to open a icecream store")
-    time.sleep(5)
+    time.sleep(3)
     print("He wants you to manange the place")
-    time.sleep(5)
+    time.sleep(3)
     print("In return he will give you 5 million dollars!")
+    time.sleep(3)
     break
