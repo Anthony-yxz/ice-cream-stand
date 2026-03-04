7bae323b36b09ab3c91c93fef4ee9fa69f312177
Anthony-yxz <ag537413@georgetownisd.org>
2026-03-04 00:02:05 -0600

    Add files via upload

---
+++ bench234info.py
@@ -0,0 +1,18 @@
+money = 150.0
+day = 1
+
+inventory = {
+    "ice_cream": 0,
+    "cones": 0,
+    "toppings": 0
+}
+
+recipe = {
+    "ice_cream": 1,
+    "toppings": 0,
+    "cones": 1
+}
+
+price_per_item = 1.0
+
+cones_sold_today = 0
---
+++ finalcode.py
@@ -0,0 +1,54 @@
+import time
+import STORY_LINE
+import benchmark_2
+import benchmark_3
+import benchmark_4
+import bench234info
+
+benchmark_2.run_shop()
+benchmark_3.set_recipe()
+
+print("\n"*3)
+
+while True:
+
+    if bench234info.money <= 0:
+        print("You ran out of money bro but ill spare you a 50\n","but can you please pay me back later")
+        time.sleep(1.5)
+        bench234info.money =+ 50
+    if bench234info.day > 7:
+        print("Congratulations! You survived 7 days!")
+        time.sleep(1.5)
+        break   
+
+    print("\n" * 2)
+    print("day",bench234info.day)
+    print("Thank you for setting up your stand!")
+    print("What would you like to do from here?")
+    time.sleep(1.5)
+    print("\n" * 2)
+    print("1  Go back to shop")
+    print("2  Change your recipe")
+    print("3  START DAY")
+    print("4  learn how to play")
+
+    choice = input("Enter the number of your choice: ")
+
+    if choice == "1":
+        benchmark_2.run_shop()
+
+    elif choice == "2":
+        benchmark_3.set_recipe()
+        
+
+    elif choice == "3":
+        benchmark_4.run_day()
+        bench234info.day += 1
+
+        
+    elif choice == "4":
+        print("this is your ice cream standn\n","your goal is to survive 7 days without running out of money\n","you can buy from the shop,change your recipe,or start your day\n")
+        time.sleep(1.5)
+    else:
+        print("atleast play the game right, enter 1, 2, 3, or 4 or click ctrl+C")
+        time.sleep(1)
